3f3f4b3d30d8293e5241dfc2181f76b5b3008f21
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-01-25 13:28:47 +0000

    Add App Gems & Training Forms

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,4 +1,40 @@
 class TrainingsController < ApplicationController
+	before_action :find_training, only: [:show, :edit, :update, :destroy]
 	def index
+	end
+
+	def show
+	end
+
+	def new
+		@training = Training.new
+	end
+
+	def create
+		@training = Training.new(training_params)
+		if @training.save
+			redirect_to @training
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def training_params
+		params.require(:training).permit(:date, :workout, :length)
+	end
+
+	def find_training
+		@training = Training.find(params[:id])
 	end
 end
